ba308183a5166427f087197e90746dcb6bce6a55
Samyak Jhanjhari <jhanjhari.samyak@gmail.com>
2025-04-25 16:21:12 +0530

    changes made in high card

--- Functions/high_card.js
+++ Functions/high_card.js
@@ -1,40 +1,40 @@
-const hand1 = [
-  { rank: "A", suit: "spades" },
-  { rank: "K", suit: "spades" },
-  { rank: "Q", suit: "spades" },
-  { rank: "J", suit: "spades" },
-  { rank: "10", suit: "spades" },
-];
+// const hand1 = [
+//   { rank: "A", suit: "spades" },
+//   { rank: "K", suit: "spades" },
+//   { rank: "Q", suit: "spades" },
+//   { rank: "J", suit: "spades" },
+//   { rank: "10", suit: "spades" },
+// ];
 
+function highCard(hand) {
+  const ranks = hand.map((card) => card.rank); // creates an array of ranks
+  const rankvalues = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14,
+  }; // creates an array to give you the numerical value of the rank
 
-function highCard(hand){
-    const ranks = hand.map((card) => card.rank); // creates an array of ranks
-    const rankvalues = {
-        2:2,
-        3:3,
-        4:4,
-        5:5,
-        6:6,
-        7:7,
-        8:8,
-        9:9,
-        10:10,
-        J:11,
-        Q:12,
-        K:13,
-        A:14,
-    };// creates an array to give you the numerical value of the rank
-
-    const values = ranks.map((rank) => rankvalues[rank]);// creates an array which includes all the numerical value of the rannks
-    let high = 2;
-    let index;
-    for(let i = 0 ; i<values.length ; i++){ // loop to check the high card
-        if(high < values[i]){
-            high = values[i]; // stores the high card value
-            index = i; // stores the index value of high card
-        }
+  const values = ranks.map((rank) => rankvalues[rank]); // creates an array which includes all the numerical value of the rannks
+  let high = 2;
+  let index;
+  for (let i = 0; i < values.length; i++) {
+    // loop to check the high card
+    if (high < values[i]) {
+      high = values[i]; // stores the high card value
+      index = i; // stores the index value of high card
     }
-    return hand[index];
+  }
+  return hand[index];
 }
 
-highCard(hand1);
+// highCard(hand1);
