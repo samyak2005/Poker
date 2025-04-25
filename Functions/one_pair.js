117093eeb2efc4c0fe806d1929797fb18b2264af
Shreyasreddy2006 <shreyas.24bcs10152@sst.scaler.com>
2025-04-25 16:53:26 +0530

    one_pair function updated

---
+++ Functions/one_pair.js
@@ -0,0 +1,26 @@
+const hand1 = [
+    { rank: "A", suit: "spades" },
+    { rank: "K", suit: "spades" },
+    { rank: "Q", suit: "spades" },
+    { rank: "Q", suit: "spades" },
+    { rank: "J", suit: "spades" },
+];
+
+function onePair(hand){
+    const ranks = hand.map((card) => card.rank); // creates an array of ranks
+    let map = new Map(); //  creates a new hashmap
+    for(let i =0;i<ranks.length;i++){ // loop to add ranks in map
+       if(map.has(ranks[i])){
+         map.set(ranks[i] , map.get(ranks[i])+1);// increments the count of cards if repeated
+       }else{
+         map.set(ranks[i] , 1);// adds rank with initial count 1 to the map
+       }
+    }
+    for(let i = 0;i<ranks.length;i++){
+        if(map.get(ranks[i]) == 2){
+            return hand[i];// returns the card which is repeated once in the hand
+        }
+    }
+}
+
+onePair(hand1);
