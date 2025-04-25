1e814b4d9c07393617f33dcfd7532e95bad92837
Samyak Jhanjhari <jhanjhari.samyak@gmail.com>
2025-04-25 17:00:47 +0530

    added straight.js

---
+++ Functions/straight.js
@@ -0,0 +1,48 @@
+// const hand1 = [
+//   { rank: "A", suit: "spades" },
+//   { rank: "2", suit: "spades" },
+//   { rank: "3", suit: "spades" },
+//   { rank: "4", suit: "diamonds" },
+//   { rank: "5", suit: "spades" },
+// ];
+
+// NOTE A2345 comes in straight
+
+function arraysEqual(arr1, arr2) {
+  return arr1.every((val, i) => val === arr2[i]);
+}
+
+function isStraightFlush(hand) {
+  const ranks = hand.map((card) => card.rank);
+
+  const rankValues = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14,
+  };
+
+  const values = ranks.map((rank) => rankValues[rank]);
+  values.sort((a, b) => a - b);
+
+  let isStraight = values.every((val, i, arr) => {
+    if (i === 0) return true;
+    return val === arr[i - 1] + 1;
+  });
+
+  const aceLow = [2, 3, 4, 5, 14];
+  const ace_case = arraysEqual(values, aceLow);
+
+  return isStraight || ace_case;
+}
+
+// console.log(isStraightFlush(hand1));
