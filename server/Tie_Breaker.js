7e000f3739cb16228f9f9fce69c2399a798f1fb1
Samyak Jhanjhari <jhanjhari.samyak@gmail.com>
2025-04-27 18:27:35 +0530

    updated Tie_Breaker.js

--- server/Tie_Breaker.js
+++ server/Tie_Breaker.js
@@ -4,23 +4,6 @@ const { TwoPair } = require("./Tie_Breaker_Functions/Two_pair.js");
 const { ThreeKind } = require("./Tie_Breaker_Functions/Three_kind.js");
 const { FourKind } = require("./Tie_Breaker_Functions/Four_Kind.js");
 const { FullHouse } = require("./Tie_Breaker_Functions/Full_House.js");
-
-// const hand1 = [
-//     {rank : "2" , suit : "spades"},
-//     {rank : "2" , suit : "spades"},
-//     {rank : "3" , suit : "spades"},
-//     {rank : "4" , suit : "spades"},
-//     {rank : "5" , suit : "spades"},
-// ]
-
-// const hand2 = [
-//     {rank : "K" , suit : "spades"},
-//     {rank : "K" , suit : "spades"},
-//     {rank : "2" , suit : "spades"},
-//     {rank : "3" , suit : "spades"},
-//     {rank : "4" , suit : "spades"},
-// ]
-
 /**
  * @param {Integer} priority - Integer value of priority
  * @param {Array} hand1 - Array of 5 card objects, each with rank and suit properties.
@@ -28,42 +11,42 @@ const { FullHouse } = require("./Tie_Breaker_Functions/Full_House.js");
  * @returns {Array} - Higher hand among two hands with same priority.
  */
 
-function TieBreaker(priority , hand1 , hand2){
-    if(priority == 10){
-        return HighCard(hand1,hand2);
-    }
-
-   if(priority == 9){
-      return OnePair(hand1,hand2);
-   }
-   
-   if(priority == 8){
-     return TwoPair(hand1,hand2);
-   }
-   
-   if(priority == 7){
-     return ThreeKind(hand1,hand2);
-   }
-   
-   if(priority == 6){
-     return HighCard(hand1,hand2);
-   }
-   
-   if(priority == 5){
-    return HighCard(hand1,hand2);
-  }
-  
-  if(priority == 4){
-    return FullHouse(hand1,hand2);
+function TieBreaker(priority, hand1, hand2) {
+  if (priority == 10) {
+    return HighCard(hand1, hand2);
   }
 
-  if(priority == 3){
-    return FourKind(hand1,hand2);
+  if (priority == 9) {
+    return OnePair(hand1, hand2);
   }
 
-  if(priority == 2){
-    return HighCard(hand1,hand2);
+  if (priority == 8) {
+    return TwoPair(hand1, hand2);
+  }
+
+  if (priority == 7) {
+    return ThreeKind(hand1, hand2);
+  }
+
+  if (priority == 6) {
+    return HighCard(hand1, hand2);
+  }
+
+  if (priority == 5) {
+    return HighCard(hand1, hand2);
+  }
+
+  if (priority == 4) {
+    return FullHouse(hand1, hand2);
+  }
+
+  if (priority == 3) {
+    return FourKind(hand1, hand2);
+  }
+
+  if (priority == 2) {
+    return HighCard(hand1, hand2);
   }
 }
 
-// console.log(TieBreaker(2,hand1,hand2));
+module.exports = { TieBreaker };
