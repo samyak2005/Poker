fdca0998bb94b3bc15624a9e8823630383e7b132
jenyyy4 <anuskaroy0410@gmail.com>
2025-05-01 01:23:26 +0530

    updated help component

--- client/src/components/Help.jsx
+++ client/src/components/Help.jsx
@@ -3,9 +3,9 @@ import { faClose } from '@fortawesome/free-solid-svg-icons';
 
 const Help = ({ close }) => {
     return (
-       <div className="flex flex-col justify-center items-center gap-2 h-screen relative z-2">
-            <div className="h-9 w-9 flex items-center justify-center rounded-full bg-[#1C023F] hover:bg-[#3a0ca3] cursor-pointer" onClick={close}>
-                <FontAwesomeIcon icon={faClose} className="text-lg text-white" />
+       <div className="flex flex-col justify-center items-center h-screen relative z-2">
+            <div className="h-10 w-10 flex items-center justify-center rounded-full hover:bg-[#281B4E] text-white cursor-pointer absolute top-[18%] right-[6%]" onClick={close}>
+                <FontAwesomeIcon icon={faClose} className="text-2xl" />
             </div>
             <div className="h-[80vh]">
                 <img src="help.png" className="h-full" draggable="false"/>
